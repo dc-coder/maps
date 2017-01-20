bc6abd32ec9256cbd0479e4f4e5cc884208c7204
dc-coder <mir.mir@gmail.com>
2017-01-20 01:47:27 -0500

    adding data summary, popup marker, better styling

--- ui.js
+++ ui.js
@@ -30,14 +30,41 @@ var map = new mapboxgl.Map({
 
 
 
+
 map.on('load', function() {
+
+  map.addSource("startups", {
+    "type": "geojson",
+    'data': 'data/' + $('.active').attr("datafile-rootname") + 'all.json'
+  });
+
   map.addLayer({
-    'id': 'startups',
+    'id': 'startups-fill',
     'type': 'fill-extrusion',
-    'source': {
-      'type': 'geojson',
-      'data': 'data/' + $('.active').attr("datafile-rootname") + 'all.json'
-    },
+    'source': 'startups',
+    'paint': {
+      'fill-extrusion-color': {
+        'property': 'total',
+        'type': 'exponential',
+        'stops': [
+          [1, "#EC407A"],
+          [1500, "#4A148C"]
+        ]
+      },
+      'fill-extrusion-height': {
+        'property': 'total',
+        'type': 'identity'
+      },
+      // Make extrusions slightly opaque for see through
+      'fill-extrusion-opacity': 0.85
+    }
+  });
+
+
+  map.addLayer({
+    'id': 'startups-fill-hover',
+    'type': 'fill-extrusion',
+    'source': 'startups',
     'paint': {
       'fill-extrusion-color': {
         'property': 'total',
@@ -53,17 +80,142 @@ map.on('load', function() {
       },
       // Make extrusions slightly opaque for see through
       'fill-extrusion-opacity': 0.9
-    }
+    },
+    "filter": ["==", "total", ""]
   });
+
+   map.addLayer({
+    "id": "startups-details", 
+    'type': 'circle',
+    "source": {
+      "type": "geojson",
+      'data': 'data/' + $('.active').attr("datafile-rootname") + '-details.min.geojson' 
+      } ,
+      "paint": {
+        "circle-opacity" : 0
+      }
+  });
+
+
 });
 
+// When a click event occurs near a polygon, open a popup at the location of
+// the feature, with description HTML from its properties.
+map.on('click', function(e) {
+
+  if (!map.loaded()) return;
+
+  var features = map.queryRenderedFeatures(e.point, {
+    layers: ['startups-fill']
+  });
+  if (!features.length) {
+    return;
+  }
+
+  var feature = features[0];
+
+  var markerHeight = feature.properties.total, markerRadius = 10, linearOffset = 25;
+ 
+  // I need to fix the offsets of the popup... so each one shows based on where the marker height is
+  // Also need to close the marker when navigating the year or moving on the map.
+  // Popup only closes when another one is opened. try popup.remove again.
+  // var popupOffsets = {
+  //  'top': [0, 0],
+  //  'top-left': [0,0],
+  //  'top-right': [0,0],
+  //  'bottom': [0, - markerHeight  ],
+  //  'bottom-left': [linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+  //  'bottom-right': [-linearOffset, (markerHeight - markerRadius + linearOffset) * -1],
+  //  'left': [markerRadius, (markerHeight - markerRadius) * -1],
+  //  'right': [-markerRadius, (markerHeight - markerRadius) * -1]
+  //  };
+  // var popup = new mapboxgl.Popup({offset:popupOffsets})
+
+
+
+  var popup = new mapboxgl.Popup();
+
+  popup.setLngLat(map.unproject(e.point));
+  popup.setHTML("<div class='popup-text'>startup activity<br> <br>" + feature.properties.total + "</div>");
+  popup.addTo(map);
+
+
+});
+
+// Use the same approach as above to indicate that the symbols are clickable
+// by changing the cursor style to 'pointer'.
+map.on("mousemove", function(e) {
+
+  if (!map.loaded()) return;
+
+  var features = map.queryRenderedFeatures(e.point, {
+    layers: ["startups-fill"]
+  });
+  if (features.length) {
+    map.setFilter("startups-fill-hover", ["==", "total", features[0].properties.total]);
+  } else {
+    map.setFilter("startups-fill-hover", ["==", "total", ""]);
+  }
+
+});
+
+
+// Reset the state-fills-hover layer's filter when the mouse leaves the map
+map.on("mouseout", function() {
+
+  if (!map.loaded()) return;
+  map.setFilter("startups-fill-hover", ["==", "total", ""]);
+
+});
+
+
+ 
+ map.on('moveend', function() {   
+    
+
+      var features = map.queryRenderedFeatures({layers:['startups-details']});   
+   
+       if (features) { 
+
+         var obj = { };
+
+       
+          features.forEach(function(feature) {    
+          // console.log(feature.properties.e);
+             
+             obj[feature.properties.e] = (obj[feature.properties.e] || 0) + 1;
+ 
+    
+       });        
+  
+   
+}   
+
+     
+  $(".nav-side-desc p").html("");
+
+  $.each( obj, function( key, value ) {
+    $(".nav-side-desc p").append(key.substr(0,1).toUpperCase() + key.substr(1) +  "<br> " +  value + "<br><br>"); 
+  });
+
+  
+   
+   });    
+ 
+
+
+// map.on('source.loading', function (e) {
+//   console.log(e.status); // one of 'fetching', 'idle', 'errored'
+// });
+ 
+ 
 
 $(".cities").on('click', ".city", function() {
   //change which city is active
   $(".city").removeClass("active");
   $(this).addClass("active");
   //fly to destination, i could use pan to to avoid blank map or load map in background
-  map.flyTo({
+  map.easeTo({
     center: JSON.parse('[' + $(this).attr("center") + ']')
   });
   //set the source of the layer to new city data
@@ -75,11 +227,13 @@ $(".cities").on('click', ".city", function() {
 
 
 $(".next").click(function() {
+
   var $toHighlight = $('.selected').next().length > 0 ? $('.selected').next() : $('ul#filters li').first();
   $('.selected').removeClass('selected');
   $toHighlight.addClass('selected');
   selectedYear = $toHighlight.text().trim();
-  $("#year-display").text(selectedYear);
+
+  $(".nav-side-year-display").html(selectedYear);
   setYear(selectedYear)
 });
 
@@ -90,7 +244,7 @@ $(".prev").click(function() {
   $('.selected').removeClass('selected');
   $toHighlight.addClass('selected');
   selectedYear = $toHighlight.text().trim();
-  $("#year-display").text(selectedYear);
+  $(".nav-side-year-display").text(selectedYear);
   setYear(selectedYear)
 });
 
