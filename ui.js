099017fb2f1c402aca0f0a210ac39f395e3aeb6b
dc-coder <mir.mir@gmail.com>
2017-02-22 21:37:36 -0500

    new map style for timeline view only

--- ui.js
+++ ui.js
@@ -1,41 +1,40 @@
 //generate list of cities and their properties from config.json
 $.getJSON("ui-settings.json", function(data) {
+
   $.each(data, function(key, val) {
     //add the li
     $(".cities").append("<div class='city item' id='city-" + key + "'> <div class='text'>" + val["name"] + "</div></div>");
+
     //loop and add each attribute 
     $.each(val, function(attr) {
       //generate the custom attributes
       $("#city-" + key).attr(attr, val[attr]);
-      //set the background image
-      $("#city-" + key).css('background-image', 'url(' + val["img"] + ')');
+      //set the background image      
     });
   });
+
   //set the city with selected=1 to active
   $("[loaded=1]").addClass("active");
   years = '<li>' + $("[loaded=1]").attr("years").replace(/,/gi, '</li><li>') + '</li><';
   $("ul#filters").html(years);
   $("ul#filters li:last-child").addClass("selected");
+
 });
 
-$('.infinite-carousel').infiniteCarousel({
-  itemsPerMove : 2,
-  duration : 500,
-  vertical : true
-});
-
+ 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWlyZWlsbGVyYWFkIiwiYSI6ImZSQURPM3cifQ.fivqJpti-Um8m38RMPWzkQ';
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/dark-v9',
-  center: [-73.93951177597046,40.72475450489242],
+  center: [-73.99283409118652, 40.75041060083396],
   zoom: 11.5,
   pitch: 60
 });
 
+var nav = new mapboxgl.NavigationControl();
+map.addControl(nav, 'top-left');
 
-
-
+ 
 map.on('load', function() {
 
   map.addSource("startups", {
@@ -49,7 +48,7 @@ map.on('load', function() {
     'source': 'startups',
     'paint': {
       'fill-extrusion-color': {
-        'property': 'percentage_vis',
+        'property': 'total_vis',
         'type': 'exponential',
         'stops': [
         [1, "#EC407A"],
@@ -57,47 +56,11 @@ map.on('load', function() {
         ]
       },
       'fill-extrusion-height': {
-        'property': 'percentage_vis',
+        'property': 'total_vis',
         'type': 'identity'
       },
       // Make extrusions slightly opaque for see through
       'fill-extrusion-opacity': 0.9
-    }
-  });
-
-
-  // map.addLayer({
-  //   'id': 'startups-fill-hover',
-  //   'type': 'fill-extrusion',
-  //   'source': 'startups',
-  //   'paint': {
-  //     'fill-extrusion-color': {
-  //       'property': 'percentage_vis',
-  //       'type': 'exponential',
-  //       'stops': [
-  //         [1, "#EC407A"],
-  //         [1500, "#4A148C"]
-  //       ]
-  //     },
-  //     'fill-extrusion-height': {
-  //       'property': 'percentage_vis',
-  //       'type': 'identity'
-  //     },
-  //     // Make extrusions slightly opaque for see through
-  //     'fill-extrusion-opacity': 0.9
-  //   },
-  //   "filter": ["==", "percentage_vis", ""]
-  // });
-
-  map.addLayer({
-    "id": "startups-details", 
-    'type': 'circle',
-    "source": {
-      "type": "geojson",
-      'data': 'data/details/' + $('.active').attr("datafile-rootname") + '-details.csv.min.geojson' 
-    } ,
-    "paint": {
-      "circle-opacity" : 0
     }
   });
 
@@ -131,7 +94,7 @@ map.on('mousemove', function(e) {
 
  var feature = features[0];
 
- var markerHeight = feature.properties.percentage_vis, markerRadius = 10, linearOffset = 25;
+ var markerHeight = feature.properties.total_vis, markerRadius = 10, linearOffset = 25;
  
   // I need to fix the offsets of the popup... so each one shows based on where the marker height is
   // Also need to close the marker when navigating the year or moving on the map.
@@ -148,196 +111,78 @@ map.on('mousemove', function(e) {
   //  };
   // var popup = new mapboxgl.Popup({offset:popupOffsets})
 
-
-
-
+ 
   popup.setLngLat(map.unproject(e.point));
   popup.setHTML("<div class='popup-text'>startup activity<br>" + feature.properties.total + "</div>");
   popup.addTo(map);
 
+  console.log(feature);
 
 });
 
-// Use the same approach as above to indicate that the symbols are clickable
-// by changing the cursor style to 'pointer'.
-// map.on("mousemove", function(e) {
-
-//   if (!map.loaded()) return;
-
-//   // var features = map.queryRenderedFeatures(e.point, {
-//   //   layers: ["startups-fill"]
-//   // });
-//   // if (features.length) {
-//   //   map.setFilter("startups-fill-hover", ["==", "percentage_vis", features[0].properties.percentage_vis]);
-//   // } else {
-//   //   map.setFilter("startups-fill-hover", ["==", "percentage_vis", ""]);
-//   // }
-
-// });
-
-
-// Reset the state-fills-hover layer's filter when the mouse leaves the map
-// map.on("mouseout", function() {
-
-//   if (!map.loaded()) return;
-//   map.setFilter("startups-fill-hover", ["==", "percentage_vis", ""]);
-
-// });
-
-
-// var datapoints = [];
-
-// var filterEl = document.getElementsByClassName('selected');
-// var listingEl = document.getElementsByClassName('nav-side-desc p');
-//  var obj = { };
-
-// function renderListings(features) {
-//     // Clear any existing listings
-//     listingEl.innerHTML = '';
-//     if (features.length) {
-//         features.forEach(function(feature) {
-//             obj[feature.properties.e] = (obj[feature.properties.e] || 0) + 1; 
-//             $.each( obj, function( key, value ) {
-//              item = key.substr(0,1).toUpperCase() + key.substr(1) +  "<br> " +  value + "<br><br>";
-//             });
-
-//             $(".nav-side-desc p").html(item);
-//         });
-
-        
-//     } else {
-       
-//         // remove features filter
-//          map.setFilter('startups-details', ['==', 'm', yr]);
-//     }
-// }
-
-
-//   map.on('moveend', function() {
-//         var features = map.queryRenderedFeatures({layers:['startups-details']});
-
-//         if (features) {
-           
-//             // Populate features for the listing overlay.
-//             renderListings(features);
-
-//             // Clear the input container
-//             filterEl.value = '';
-
-             
-//         }
-//     });
-
-
-
-
-map.on('moveend', function() {   
-
-  yr = $(".selected").text().trim();
-  setNavText(yr);
-
-
-
-});    
-
-
-
-// map.on('source.loading', function (e) {
-//   console.log(e.status); // one of 'fetching', 'idle', 'errored'
-// });
-
+   
 
 
 $(".cities").on('click', ".city", function() {
   //change which city is active
   $(".city").removeClass("active");
   $(this).addClass("active");
+  $("#current-year").text("");
+
   //fly to destination, i could use pan to to avoid blank map or load map in background
   map.easeTo({
     center: JSON.parse('[' + $(this).attr("center") + ']')
   });
   //set the source of the layer to new city data
   map.getSource("startups").setData('data/hex/' + $('.active').attr("datafile-rootname").trim() + 'all.json');
-  map.getSource("startups-details").setData('data/details/' + $('.active').attr("datafile-rootname").trim() + '-details.csv.min.geojson');
+  
   years = '<li>' + $(this).attr("years").replace(/,/gi, '</li><li>') + '</li><';
   $("ul#filters").html(years);
 
 });
 
+ 
 
-
-$(".next-year").click(function() {
-
-  var $toHighlight = $('.selected').next().length > 0 ? $('.selected').next() : $('ul#filters li').first();
-  $('.selected').removeClass('selected');
-  $toHighlight.addClass('selected');
-  selectedYear = $toHighlight.text().trim();
-
-  $(".nav-side-year-display").html(selectedYear);
-  setYear(selectedYear);
-
-});
+$("#play-pause-year").click(function() {
 
 
 
-$(".prev-year").click(function() {
-  var $toHighlight = $('.selected').prev().length > 0 ? $('.selected').prev() : $('ul#filters li').last();
-  $('.selected').removeClass('selected');
-  $toHighlight.addClass('selected');
-  selectedYear = $toHighlight.text().trim();
-  $(".nav-side-year-display").text(selectedYear);
-  setYear(selectedYear);
-});
+
+
+
+ if( $("#play-pause-year").text() == "play_arrow") {
+   
+    $("#play-pause-year").text("pause_arrow");
+    
+laInterval = setInterval(function(){
+
+   var $toHighlight = $('.selected').next().length > 0 ? $('.selected').next() : $('ul#filters li').first();
+    $('.selected').removeClass('selected');
+    $toHighlight.addClass('selected');
+    selectedYear = $toHighlight.text().trim();
+    if (selectedYear =="all") {
+      clearInterval(laInterval);
+    }
+    setYear(selectedYear);
+    $("#current-year").text(selectedYear);
+   }, 1000);
+  
+ } else {
+  clearInterval(laInterval);
+   $("#play-pause-year").text("play_arrow");
+    
+  
+    console.log("stop ya");
+ }
+
+
+
+}) 
 
 function setYear(yr) {
-
-
+ 
 
   map.getSource("startups").setData('data/hex/' + $('.active').attr("datafile-rootname").trim() + yr + '.json');
-  map.getSource("startups-details").setData('data/details/' + $('.active').attr("datafile-rootname").trim() + '-details.csv.min.geojson');
   
-
- 
-
 
 };
-
-function setNavText(yr) {
-
-
-  if (yr =="all"){
-     map.setFilter('startups-details', ['>', 'm', '0']);
-  }else {
-    map.setFilter('startups-details', ['==', 'm', yr]);
-  }
-  
- 
-
- var obj = { };
-
-  var features = map.queryRenderedFeatures({layers:['startups-details']});   
-
-  if (features) { 
-
-   var obj = { };
-
-
-   features.forEach(function(feature) {    
-          // console.log(feature.properties.e);
-
-          obj[feature.properties.e] = (obj[feature.properties.e] || 0) + 1;
-
-
-        });        
-
-   
- }   
-
-
- $(".nav-side-desc p").html("");
-
- $.each( obj, function( key, value ) {
-  $(".nav-side-desc p").append(key.substr(0,1).toUpperCase() + key.substr(1) +  "<br> " +  value + "<br><br>"); 
-});
-
-}
