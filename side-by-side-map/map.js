cbce31ffe66a68786e96b4e1a66d5020bd40718f
dc-coder <mir.mir@gmail.com>
2017-03-05 19:45:18 -0500

    legends, filters ui

--- side-by-side-map/map.js
+++ side-by-side-map/map.js
@@ -12,13 +12,16 @@ var afterMap = new mapboxgl.Map({
     container: 'startup-data',
     style: 'mapbox://styles/mapbox/light-v9',
     center: [-73.99283409118652, 40.75041060083396],
-    zoom: 11
+    zoom: 11 , 
+    preserveDrawingBuffer: true
 });
 
 
 var map = new mapboxgl.Compare(beforeMap, afterMap, {
     // Set this to enable comparing two maps by mouse movement:
 });
+
+ 
 
 
 //'mapbox://styles/mireilleraad/cizkf1gk7000p2smzpgrtd6x1'
@@ -49,7 +52,7 @@ afterMap.on('click', function(e) {
             fullscreenControlOptions: {
                 position: google.maps.ControlPosition.LEFT_TOP
             },
-            zoom: 2,
+            zoom: 1,
             visible: true,
             mode: 'html5' // fixes bug in FF: http://stackoverflow.com/questions/28150715/remove-google-street-view-panorama-effect
         });
@@ -57,34 +60,45 @@ afterMap.on('click', function(e) {
 });
 
 
-afterMap.on('load' , function() {
+function setMapData(city) {
 
+    console.log(city);
+    if (city == "startup city") { 
+        city = "nyc";
+    } else 
+    {
+        afterMap.removeSource("startups");
+        afterMap.removeLayer("cluster-1");
+        afterMap.removeLayer("cluster-2");
+        afterMap.removeLayer("cluster-3");
+        afterMap.removeLayer("cluster-4");
+        afterMap.removeLayer("cluster-5");
+        afterMap.removeLayer("unclustered-points");
+        afterMap.removeLayer("cluster-count");
+    }
+ 
+ 
 
-    afterMap.addSource("nyc-startups", {
+    afterMap.addSource("startups", {
+       
         type: "geojson",
-        // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
-        // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-        data: "../data/details/nyc-details.csv.min.geojson",
+        data: "../data/details/" + city + "-details.csv.min.geojson",
         cluster: true,
         clusterMaxZoom: 14, // Max zoom to cluster points on
         clusterRadius: 10 // Radius of each cluster when clustering points (defaults to 50)
     });
 
-    // Use the earthquakes source to create five layers:
-    // One for unclustered points, three for each cluster category,
-    // and one for cluster labels.
+    
     afterMap.addLayer({
         "id": "unclustered-points",
         "type": "symbol",
-        "source": "nyc-startups",
+        "source": "startups",
         "filter": ["!has", "point_count"],
         "layout": {
             "icon-image": "marker-15"
         }
     });
 
-    // Display the earthquake data in three layers, each filtered to a range of
-    // count values. Each range gets a different fill color.
 
     var layers = [
         [350, '#0E2E34'],
@@ -95,20 +109,13 @@ afterMap.on('load' , function() {
         [0, '#EFEE69']
     ];
 
-    // var size = [
-    //     [350, '20'],
-    //     [250, '15'],
-    //     [150, '10'],
-    //     [20, '5'],
-    //     [20, '3'] 
-    // ];
-
 
     layers.forEach(function (layer, i) {
+ 
         afterMap.addLayer({
             "id": "cluster-" + i,
             "type": "circle",
-            "source": "nyc-startups",
+            "source": "startups",
             "paint": {
                 "circle-color": layer[1],
                 'circle-radius': 10,
@@ -125,7 +132,7 @@ afterMap.on('load' , function() {
    afterMap.addLayer({
         "id": "cluster-count",
         "type": "symbol",
-        "source": "nyc-startups",
+        "source": "startups",
         "layout": {
             "text-field": "{point_count}",
             "text-font": [
@@ -137,5 +144,70 @@ afterMap.on('load' , function() {
     });
 
 
+}
+ 
+// function getFilteredData() {
+
+
+
+// var datafile = "../data/details/nyc-details.csv.min.geojson";
+
+//   $.getJSON( datafile, {
+//     format: "json"
+//   })
+//     .done(function( data ) {
+//      var items = [];
+//       $.each( data.features, function( i, feature ) {
+
+//         //console.log(feature);
+
+//         if (feature.properties.e == "hosted" ) {
+//             items.push(feature);
+//         }
+       
+//       });
+
+//       console.log(items);
+//     });
+
+// }
+
+afterMap.on('load' , function() {
+ 
+   
+
+    var city = $("#startup-city-selected").text().toLowerCase();
+    setMapData(city);
+ 
 
 });
+
+
+ 
+
+$("#filter-by-activity").change(function(){
+
+var filters = document.getElementById('filter-by-activity').filters;
+
+var list = [];
+    for (var i = 0; i < filters.length; i++) {
+        if (filters[i].checked) list.push(filters[i].value);
+    }
+
+    console.log(list);
+  
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
--- side-by-side-map/ui.js
+++ side-by-side-map/ui.js
@@ -19,11 +19,13 @@ $("#switch-street-view").change(function() {
 
  
 $("#startup-city-list-items .mdl-menu__item").click(function(){
-
+ 
     var city = $(this).text();
+    var city_file = $(this).attr("data");
 
     var index = $( "#startup-city-list-items li" ).index( this );
  
+ 	$("#panorama").html("");
     $("#startup-city-selected").text( city) ;
 
     var center = [ "-73.99283409118652, 40.75041060083396" , "-75.57913541793823, 6.2344611857957375" , "-74.10565853118896,4.6626612070343185"];
@@ -33,7 +35,13 @@ $("#startup-city-list-items .mdl-menu__item").click(function(){
         center: JSON.parse('[' + center[index] + ']')
     });
     
-    
+    changeLegend(  $("label#startup-city-selected").text() , $("label#secondary-data-set-selected").text() );
+     
+    setMapData(city_file);
+	
+
+
+
 });
 
 
@@ -41,20 +49,44 @@ $("#secondary-data-set-list .mdl-menu__item").click(function(){
  
 
     var set = $(this).text();
+  
 
     var index = $( "#secondary-data-set-list li" ).index( this );
- 
+
+	$("#panorama").html("");
     $("#secondary-data-set-selected").text( set) ;
 
-    var le_style = ["mapbox://styles/mireilleraad/cizkf1gk7000p2smzpgrtd6x1" , "mapbox://styles/mireilleraad/cizowikhj005k2smyvk803813"]
+    var le_style = ["mapbox://styles/mireilleraad/cizkf1gk7000p2smzpgrtd6x1" , "mapbox://styles/mapbox/satellite-streets-v9" ,"mapbox://styles/mireilleraad/cizowikhj005k2smyvk803813"]
 
+    if (set =="Land Cover") {
+    	beforeMap.setZoom(13);
+    }
    beforeMap.setStyle(le_style[index]);
+
+
+   changeLegend(  $("label#startup-city-selected").text(), $("label#secondary-data-set-selected").text());
      
     
 });
 
 
+function changeLegend(city, compareto) {
+ 
 
+	if ( (city == "New York" || city =="Startup City") && compareto =="Population Density") {
+		$("#nyc-pop").show()
+	} else {
+		$("#nyc-pop").hide()
+	}
+
+	if ( (city == "New York" || city =="Startup City") && compareto =="Congestion") {
+		$("#nyc-congestion").show()
+	} else {
+		$("#nyc-congestion").hide()
+	}
+
+
+}
  
 
 
